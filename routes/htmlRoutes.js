6630bb03a84b21c9ef22b16e1476d4018e7df8b3
Jason Anderson <jas.nanders.n@gmail.com>
2021-04-10 14:13:16 -0700

    fixed htmlRoutes, apiRoutes not working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,10 @@
 const path = require("path");
 
 module.exports = function(app) {
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname + "./public/index.html"));
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
-    app.get("./notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname + "../public/index.html"));
     });
 };
-
-// not working! localhost tries to find index.html in the following (incorrect) directory:
-// C:\Users\Jason\Desktop\Homework\memopad\routes.\public\index.html
-// should be
-// C:\Users\Jason\Desktop\Homework\memopad\public\index.html
