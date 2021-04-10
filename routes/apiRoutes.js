e4de12046781065a6558fc3fb90547be93cade68
Jason Anderson <jas.nanders.n@gmail.com>
2021-04-10 09:59:01 -0700

    wrote apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,24 @@
-// get and post and DELETE requests
-// json junk
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+module.exports = (app) => {
+    app.get("./api/notes", (req, res) => {
+        console.log(JSON.stringify(data));
+        res.json(data);
+    });
+    app.post("./api/notes", (req, res) => {
+        let note = req.body;
+        note.id = uuidv4();
+        data.push(note);
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        console.log("Saved");
+        res.json(data);
+    });
+    app.delete("./api/notes/:id", (req, res) => {
+        let noteID = req.params.id.toString();
+        let newData = data.filter(note => note.id.toString() !== noteID);
+        fs.writeFileSync("./db/db.json", JSON.stringify(newData));
+        res.end(console.log("Deleted"));
+    });
+}
